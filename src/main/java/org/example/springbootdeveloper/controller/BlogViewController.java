76ea8da96e557deca81637b9a78a2d8957741a2e
sunjaehi <sunjae7944@naver.com>
2024-04-01 14:33:10 +0900

    0401 - thymeleaf

--- src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -30,7 +30,7 @@ public class BlogViewController {
     @GetMapping("/articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         Article article=blogService.findById(id);
-        model.addAttribute("article", new ArticleListViewResponse(article));
+        model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";  //article.html 뷰 조회
     }
